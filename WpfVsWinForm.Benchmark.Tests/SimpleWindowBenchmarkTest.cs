5fb8b6dfb10e4865b22361efd5447650a5b522c9
RYOHEIMIURAD197\rim <mot_siva_ordin@hotmail.com>
2018-10-24 21:28:22 +0900

    800,600のウインドウを呼び出しては閉じるベンチマーク処理を作成。

---
+++ WpfVsWinForm.Benchmark.Tests/SimpleWindowBenchmarkTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace WpfVsWinForm.Benchmark.Tests
+{
+    /// <summary>
+    /// <see cref="SimpleWindowBenchmark"/>をテストするクラスです。
+    /// </summary>
+    [TestFixture]
+    public class SimpleWindowBenchmarkTest
+    {
+        [Test]
+        public void LaunchWpfSimpleWindow()
+        {
+            var benchmark = new SimpleWindowBenchmark();
+            benchmark.LaunchWpfSimpleWindow();
+        }
+        [Test]
+        public void LaunchWinFormSimpleWindow()
+        {
+            var benchmark = new SimpleWindowBenchmark();
+            benchmark.LaunchWinFormSimpleWindow();
+        }
+    }
+}
---
+++ WpfVsWinForm.Benchmark/SimpleWindowBenchmark.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using FlaUI.UIA3;
+
+namespace WpfVsWinForm.Benchmark
+{
+    [MarkdownExporter,CsvExporter]
+    public class SimpleWindowBenchmark
+    {
+        /// <summary>
+        /// 起動
+        /// </summary>
+        /// <param name="filePath"></param>
+        public void Launch(string filePath)
+        {
+            var app = FlaUI.Core.Application.Launch(filePath);
+            using (var automation = new UIA3Automation())
+            {
+                app.WaitWhileBusy();
+                var window = app.GetMainWindow(automation);
+                window.Close();
+            }
+        }
+
+        public string RootDirectory
+        {
+            get { return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\"); }
+        }
+
+        [Benchmark]
+        public void LaunchWpfSimpleWindow()
+        {
+            string path = Path.Combine(RootDirectory, "WpfSimpleWindow", "bin", "Release", "WpfSimpleWindow.exe");
+            Launch(path);
+        }
+
+        [Benchmark]
+        public void LaunchWinFormSimpleWindow()
+        {
+            string path = Path.Combine(RootDirectory, "WinFormSimpleWindow", "bin", "Release", "WinFormSimpleWindow.exe");
+            Launch(path);
+        }
+    }
+}
